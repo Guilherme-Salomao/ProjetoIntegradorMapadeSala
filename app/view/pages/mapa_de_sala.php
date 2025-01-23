530f7769eeaf409e9e4fb75a6a778ae98fb44f78
RafaelYajima <129339723+RafaelYajima@users.noreply.github.com>
2025-01-23 17:00:07 -0300

    correção do caminho do .css

--- app/view/pages/mapa_de_sala.php
+++ app/view/pages/mapa_de_sala.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Senac Araçatuba - Gestão</title>
-    <link rel="stylesheet" href="mapa_de_sala.css">
+    <link rel="stylesheet" href="../../../assets/css/mapa_de_sala.css">
 
 </head>
 
