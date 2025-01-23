9dd6a77839826bc3dd0fc9727192926edc2c53b1
Guilherme Salomão Shorane <64329822+Guilherme-Salomao@users.noreply.github.com>
2025-01-23 13:53:47 -0300

    Volta as Aulas
    Organizando o projeto.

---
+++ index1.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- NOME PADRÃO DO SISTEMA -->
+    <title>Mapa de Sala - Senac Araçatuba</title>
+    <!--FOLHA DE ESTILO PADRÃO DO SISTEMA -->
+    <link rel="stylesheet" href="css/mapadesala.css">
+</head>
+
+<body>
+
+    <?php
+    // Cabecalho da pagina
+    include 'assets/global/header.php';
+    include 'assets/global/sidebar.php';
+    ?>
+
+</body>
+
+</html>
